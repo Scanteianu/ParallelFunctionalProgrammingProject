10565e992d268f53ca9081552f654e305f5e6c48
Stanley Ye <yy2922@columbia.edu>
2020-12-05 00:39:05 -0500

    Add flag option for TfidfSingle to read a directory or a file

--- src/TfidfSingle.hs
+++ src/TfidfSingle.hs
@@ -1,26 +1,23 @@
 {-
     TFIDF project: A single thread 
 
--- Might need them 
-import System.Exit(exitFailure)
-import Data.Char ( isLower )
-import Data.Set (Set)
-import qualified Data.Set as Set
-import qualified Data.Map.Strict as Map
-import qualified Data.List as List
-
-
 $ stack ghc -- --make -Wall -O TfidfSingle.hs 
-$ ./TfidfSingle <files-path> <search-phase>
+Usage: TfidfSingle -f <filesname> <search-phase> or  TfidfSingle -d <files-path> <search-phase>
+
+"-d" flag reads each file of a directory into a Document. 
+"-f" flag reads each line of a file into a Document. 
 
 -- Test the updated the docs with tfidf 
-$./TfidfSingle "../SampleTestFiles/" "I love cat dog bird"
-[fromList [("cat",1),("i",1),("love",1)] fromList ["cat","i","love"] fromList [("cat",0.5),("i",0.3333333333333333),("love",0.3333333333333333)],fromList [("and",1),("birds",1),("cat",1),("i",1),("love",1)] fromList ["and","birds","cat","i","love"] fromList [("cat",0.5),("i",0.3333333333333333),("love",0.3333333333333333)],fromList [("dog",1),("i",1),("love",1)] fromList ["dog","i","love"] fromList [("dog",1.0),("i",0.3333333333333333),("love",0.3333333333333333)]]
+$ ./TfidfSingle -d "../SampleTestFiles/" "I love cat dog bird"
+[fromList [("cat",1),("i",1),("love",1)] fromList ["cat","i","love"] fromList [("cat",0.5),("i",0.3333333333333333),("love",0.3333333333333333)],fromList [("and",1),("birds",1),("cat",1),("i",1),("love",1)] fromList ["and","birds","cat","i","love"] fromList [("cat",0.5),("i",0.3333333333333333),("love",0.3333333333333333)],fromList [("dog",1),("fishing",1),("i",2),("love",2)] fromList ["dog","fishing","i","love"] fromList [("dog",1.0),("i",0.6666666666666666),("love",0.6666666666666666)]]
+
+$ ./TfidfSingle -f "../SampleTestFiles/file1.txt" "I love cat dog bird"
+[fromList [("dog",1),("i",1),("love",1)] fromList ["dog","i","love"] fromList [("dog",1.0),("i",0.5),("love",0.5)],fromList [("fishing",1),("i",1),("love",1)] fromList ["fishing","i","love"] fromList [("i",0.5),("love",0.5)]]
 -}
 
 
 import System.Environment(getArgs, getProgName)
-import System.Directory
+import System.Directory as Dir
 import DocumentIndexing
 import System.Exit(exitFailure)
 import System.FilePath ((</>))  
@@ -33,43 +30,90 @@ main :: IO ()
 main = do 
    args <- getArgs
    errCheckStatus <- checkInputErrors args 
-   case  errCheckStatus of 
+   case errCheckStatus of 
        1 -> do
         pn <- getProgName
-        putStr $ "Usage: "++ pn ++" <files-path> <search-phase>\n"
+        putStr $ "Usage: "++ pn ++" -f <filesname> <search-phase> or  "++ pn ++" -d <files-path> <search-phase>\n"
         exitFailure
        2 -> do
-        putStr $ "<search-phase> can only be letters.\n"
+        putStr $ "Flag Options: -f (read from a file)or -d (read from a directory)"
         exitFailure
        3 -> do
-        putStr $ "Empty directory: " ++ head args ++ "\n"
+        putStr $ "<search-phase> can only be letters.\n"
+        exitFailure
+       4 -> do
+        putStr $ "Empty directory: " ++ args !! 1 ++ "\n"
+        exitFailure
+       5 -> do
+        putStr $ "Should pass a vlid directory with -d flag.\n"
+        exitFailure     
+       6 -> do
+        putStr $ "Empty file: " ++ args !! 1 ++ "\n"
+        exitFailure
+       7 -> do
+        putStr $ "Should pass a vlid file with -f flag.\n"
         exitFailure
        _ -> do
-        files <- getDirectoryFiles $ head args
 
         -- Get all the Documents
-        docs <- readFilesToDocuments files 
+        docs <- getAllDocuments (head args) (args !! 1)
 
         -- Get all the search words 
-        let searchWordsSet =  Set.fromList $ Prelude.map sanitizeWord $ words $ args !! 1  
+        let searchWordsSet =  Set.fromList $ Prelude.map sanitizeWord $ words $ args !! 2
 
         -- Update the tfidf 
         let docsWithTfidf =  updateDocumentsWithTfIdfScore docs $ getGlobalDocumentFrequency docs searchWordsSet
 
         print docsWithTfidf 
 
+        -- TODO: select top N relevent Documents
+
+    
+-- A function that either reads from a single file to turn each line to a Document or reads from a directory to turn each file to be a Document 
+getAllDocuments :: String -> String -> IO [Document]
+getAllDocuments  flag file
+    | flag == "-d" = do 
+        files <- getDirectoryFiles file
+        readFilesToDocuments files
+    | flag == "-f" = do
+        content <- readFile file
+        return $ Prelude.map readDocument $ lines content 
+    | otherwise = return []
 
 
+-- A funtion to check if there is any input errors 
 checkInputErrors :: [String] -> IO Int 
 checkInputErrors args
-    | length args /= 2 = return 1
-    | not (validateSearchPhase (args !! 1)) = return 2
-    | otherwise = do 
-        files <- getDirectoryFiles (head args)
-        if length files == 0
-            then return 3
-            else return 0
+    | length args /= 3 = return 1
+    | not (head args == "-f" || head args == "-d")  = return 2
+    | not (validateSearchPhase (args !! 2)) = return 3
+    | otherwise = do
+        if head args == "-d"
+            then checkValidDir (args !! 1)
+            else checkValidFile (args !! 1) 
 
+checkValidDir :: String -> IO Int 
+checkValidDir dir = do 
+    isDir <- doesDirectoryExist dir 
+    if isDir
+        then do  
+            files <- getDirectoryFiles dir 
+            if length files == 0
+                then return 4
+                else return 0
+        else return 5
+
+
+checkValidFile :: String -> IO Int 
+checkValidFile file = do 
+    isFile <- doesFileExist file 
+    if isFile 
+        then do  
+            content <- readFile file 
+            if lines content == []
+                then return 6 
+                else return 0 
+        else return 7 
 
 validateSearchPhase :: [Char] -> Bool 
 validateSearchPhase [] = True 
@@ -77,11 +121,13 @@ validateSearchPhase (x : xs)
     | isLetter x || isSpace x || x == ',' = validateSearchPhase xs 
     | otherwise = False 
 
+
 -- A function to get a list of all fils in a directory 
 getDirectoryFiles :: String -> IO [FilePath] 
 getDirectoryFiles dir = do 
-    files <- Prelude.filter (\x -> x /= "." && x /= "..") <$>  getDirectoryContents dir
+    files <- Prelude.filter (\x -> x /= "." && x /= "..") <$>  Dir.getDirectoryContents dir
     return $ Prelude.map (dir </>) files
+
 
 -- A function to read all files into Documents 
 readFilesToDocuments ::[FilePath] -> IO [Document]
