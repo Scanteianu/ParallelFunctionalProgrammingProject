929b62b9748a78a10fbebed25ce8a70e419975e9
Stanley Ye <yy2922@columbia.edu>
2020-12-04 20:48:43 -0500

    Reorganize the single thread file

--- src/TfidfSingle.hs
+++ src/TfidfSingle.hs
@@ -21,23 +21,54 @@ import System.Directory
 import DocumentIndexing
 import System.Exit(exitFailure)
 import System.FilePath ((</>))  
+import Data.Char
 
 main :: IO ()
 main = do 
-   args <- getArgs 
-   case args of 
-       [dir, _] -> do
-        files <- getDirectoryFiles dir 
-        if length files == 0
-            then putStr $ "Empty directory in " ++ dir ++ "\n"
-            else do 
-                docs <- readFilesToDocuments files 
-                print docs 
-       _ -> do 
+   args <- getArgs
+   errCheckStatus <- checkInputErrors args 
+   case  errCheckStatus of 
+       1 -> do
         pn <- getProgName
-        putStr $ "Usage: "++pn++" <files-path> <search-phase>\n"
+        putStr $ "Usage: "++ pn ++" <files-path> <search-phase>\n"
         exitFailure
+       2 -> do
+        putStr $ "<search-phase> can only be letters.\n"
+        exitFailure
+       3 -> do
+        putStr $ "Empty directory: " ++ head args ++ "\n"
+        exitFailure
+       _ -> do
+        files <- getDirectoryFiles $ head args
 
+        -- Get all the Documents
+        docs <- readFilesToDocuments files 
+
+        -- Get all the search words 
+        let ws =  map sanitizeWord $ words $ args !! 1  
+
+        print ws 
+
+
+        
+
+
+checkInputErrors :: [String] -> IO Int 
+checkInputErrors args
+    | length args /= 2 = return 1
+    | not (validateSearchPhase (args !! 1)) = return 2
+    | otherwise = do 
+        files <- getDirectoryFiles (head args)
+        if null files
+            then return 3
+            else return 0
+
+
+validateSearchPhase :: [Char] -> Bool 
+validateSearchPhase [] = True 
+validateSearchPhase (x : xs)
+    | isLetter x || isSpace x || x == ',' = validateSearchPhase xs 
+    | otherwise = False 
 
 -- A function to get a list of all fils in a directory 
 getDirectoryFiles :: String -> IO [FilePath] 
@@ -54,7 +85,10 @@ readFilesToDocuments files = sequence $ helperFunc files
             where
                 readFileToString filePath = do 
                     fileContent <- readFile filePath
-                    return $ readDocument fileContent  
+                    return $ readDocument fileContent
+
+
+
 
 
 
