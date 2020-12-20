b23a5cdd2df28bb566042129a0b2d4d732d5f7c7
Stanley Ye <yy2922@columbia.edu>
2020-12-20 18:41:30 -0500

    Add comments on how to run and test the program

--- src/Tfidf.hs
+++ src/Tfidf.hs
@@ -2,19 +2,40 @@
 
 {-
 
+Link to our Github project:  https://github.com/Scanteianu/ParallelFunctionalProgrammingProject
+Link to our Google Driver folder: https://docs.google.com/presentation/d/1bvrL5256hh0KythpZ8TOJ6gy8PVsm867huHGsoqHxdw/edit#slide=id.gb24068de60_0_39
+
+
 New compilation/execution instructions for parallelism∷
-1) stack install parallel (only once)
+1) Install the package (only once)
+$ stack install parallel 
 
-2) stack ghc -- -threaded --make -Wall -O Tfidf.hs -XDeriveAnyClass -XDeriveGeneric
+2) git clone the project repo 
+$ git clone https://github.com/Scanteianu/ParallelFunctionalProgrammingProject.git
 
-import System.Directory as Dir
-    ( doesDirectoryExist, doesFileExist, getDirectoryContents )
-    ( doesDirectoryExist, doesFileExist, getDirectoryContents ) -f "../SampleTestFiles/twitterCustomerSupportTruncated.txt" "airline delay"
+2) Compile the program 
+$ stack ghc -- -threaded --make -Wall -O Tfidf.hs -XDeriveAnyClass -XDeriveGeneric
 
-3a) ./Tfidf +RTS -N4 -RTS -f "../SampleTestFiles/twitterLargeStrings.txt" "airline delay"
-3b) ./Tfidf +RTS -N4 -RTS -d "../SmallInputFiles" "author"
+3) Sample tests 
+$ ./Tfidf +RTS -N1 -RTS -f "../SampleTestFiles/file1.txt" "I love cat dog bird"
+0.000089s
+[("I love cat dog bird",2.283333333333333),("I love cat dog",1.2833333333333332),("I love cat",0.7833333333333333),("I love",0.45),("I ",0.2)]
 
-./test.sh | grep ".\|Time\|Thread"
+$ ./Tfidf +RTS -N4 -RTS -f "../SampleTestFiles/twitterLargeStrings.txt" "airline delay"
+16.384462s
+[(" checked in 2 minutes after my flight became eligible for check in and I\226\8364\8482m in boarding group C @SouthwestAir HOW https://t.co/Hc9fKCdUJK @",16.166666666666664),(" @Delta Our plane just crashed into the world trade center @Delta I have a problem with the flight I'm on right now it's urgent please respo",12.666666666666666),(" @hulu_support On live tv DVR, ~63 minutes. At Hulu online 85 via browser w/ no ads. This is first week with DVR ads and they are no-skip ad",11.083333333333332),(" @136460 Hello Kelsie, we hate to hear you having issues with our customer service. But how can I help you today? @sprintcare -im sure if u ",10.916666666666668),(" @116245 @ArgosHelpers She said sorry for the delay then buggered off. No thankyou or there's your items, just waltzed off \240\376\732\171 I was stood ",9.416666666666666)]
+
+$ ./Tfidf +RTS -N4 -RTS -d "../SmallInputFiles" "author"
+2.859438s
+[("<!DOCTYPE html>\n<html class=\"client-nojs\" lang=\"en\" dir=\"ltr\">\n<head>\n<meta charset=\"UTF-8\"/>\n<title>George R. R. Martin - Wikipedia</title>",4.5),("<!DOCTYPE html>\n<html class=\"client-nojs\" lang=\"en\" dir=\"ltr\">\n<head>\n<meta charset=\"UTF-8\"/>\n<title>A Song of Ice and Fire - Wikipedia</tit",3.25),("\n\n\n\nGame of Thrones - Wikipedia\ndocument.documentElement.className=\"client-js\";RLCONF={\"wgBreakFrames\":!1,\"wgSeparatorTransformTable\":[\"\",\"\"",1.5),("<!DOCTYPE html>\n<html class=\"client-nojs\" lang=\"en\" dir=\"ltr\">\n<head>\n<meta charset=\"UTF-8\"/>\n<title>A Dance with Dragons - Wikipedia</title",1.25),("<!DOCTYPE html>\n<html class=\"client-nojs\" lang=\"en\" dir=\"ltr\">\n<head>\n<meta charset=\"UTF-8\"/>\n<title>Winter Is Coming - Wikipedia</title>\n<s",0.0)]
+
+
+4) Run the experiment with a simple shell program 
+4.a) Download 100ktwitter.txt, 500ktwitter.txt, 1mktwitter.txt from the Google Drive folder and move them to SampleTestFiles folder. These files are too large to push to github repo. 
+4.b) ./test.sh | grep ".\|Time\|Thread"
+4.c) Note the above experiment only captures the maxAndSort parallel part. 
+
+
 -}
 
 import System.Environment(getArgs, getProgName)
@@ -58,8 +79,8 @@ main = do
        _ -> do
 
         results <- runTfidfNoPrint (head args) (args!!1) (args !! 2) 
-        print $ results
-        -- putStr "\n"
+        --print $ results
+        putStr "\n"
 
 runTfidfNoPrint :: String -> String -> String -> IO [(String,Double)]
 runTfidfNoPrint flag files keys = do
